d2a112490b016e1b620c3f2d73170383e1e4b08b
aayushman051 <aayushmansingh051@gmail.com>
2026-05-06 02:33:42 +0530

    excption 2nd qyes

--- python_basic/Exception_Handling/Exercise.py
+++ python_basic/Exception_Handling/Exercise.py
@@ -6,4 +6,23 @@ try:
 except Exception as e:
     print("Exception occured",e)
      
+#Q2 You have certain employee records as a list of dictionaries. The dictionary contains the following information.
+employees = [
+    {"id": 1, "first_name": "Virat", "middle_name": "Kumar", "last_name": "Kohli"},
+    {"id": 2, "first_name": "Rohit", "last_name": "Sharma"},
+    {"id": 3, "first_name": "MS", "middle_name": "Singh", "last_name": "Dhoni"}
+]
 
+for employee in employees:
+
+    try:
+        full_name = (
+            employee['first_name'] + " " +
+            employee['middle_name'] + " " +
+            employee['last_name']
+        )
+
+    except Exception as e:
+        print("exception is: ",e)
+
+    print(full_name)
